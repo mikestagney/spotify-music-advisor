a1ac22594bd4c85119e976359d1a62bb6753f6f9
mikestagney <mikestagney@yahoo.com>
2021-05-26 15:27:43 -0400

    moved main menu loop into usermenu class

---
+++ UserMenu.java
@@ -0,0 +1,66 @@
+package advisor;
+
+import java.util.Scanner;
+
+public class UserMenu {
+    Scanner input;
+    boolean keepGoing;
+    String userChoice;
+
+    UserMenu() {
+        input = new Scanner(System.in);
+        keepGoing = true;
+    }
+
+    public void runCLI() {
+        do {
+        userChoice = input.nextLine();
+        switch (userChoice) {
+            case("new"):
+                System.out.println(newReleases());
+                break;
+            case("featured"):
+                System.out.println(featured());
+                break;
+            case("categories"):
+                System.out.println(categories());
+                break;
+            case("playlists Mood"):
+                System.out.println(moodPlaylists());
+                break;
+            case("exit"):
+                System.out.println("---GOODBYE!---");
+                keepGoing = false;
+                break;
+        }
+    } while (keepGoing);
+}
+    private String newReleases() {
+        return "---NEW RELEASES---\n" +
+                "Clockwork Angels [Rush]\n" +
+                "Fear Inoculum [Tool]\n" +
+                "Apex [Unleash The Archers]\n" +
+                "Liquid Tension Experiment 3 [Liquid Tension Experiment]";
+    }
+    private String featured() {
+        return "---FEATURED---\n" +
+                "Songs to wake up to\n" +
+                "Songs you don't want to wake up to\n" +
+                "None of the junk you hear on the radio\n" +
+                "Pop (radio junk)";
+    }
+    private String categories() {
+        return "---CATEGORIES---\n" +
+                "Rock\n" +
+                "Rap\n" +
+                "Funk\n" +
+                "No Country for old men";
+    }
+    private String moodPlaylists() {
+        return "---MOOD PLAYLISTS---\n" +
+                "Workout Time\n" +
+                "Before the Game\n" +
+                "Love in the Air\n" +
+                "Last line of the project";
+    }
+}
